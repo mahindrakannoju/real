3dd9b676a2704266b87a57c1b42286f39cd74c7e
mahindrakannoju <mahindrakannoju@gmail.com>
2025-08-30 11:00:57 +0530

    Add files via upload

---
+++ StudyMate.py
@@ -0,0 +1,22 @@
+from sentence_transformers import SentenceTransformer, util
+
+model_path = "ibm-granite/granite-embedding-english-r2"
+# Load the Sentence Transformer model
+model = SentenceTransformer(model_path)
+
+input_queries = [
+    ' Who made the song My achy breaky heart? ',
+    'summit define'
+    ]
+
+input_passages = [
+    "Achy Breaky Heart is a country song written by Don Von Tress. Originally titled Don't Tell My Heart and performed by The Marcy Brothers in 1991. ",
+    "Definition of summit for English Language Learners. : 1 the highest point of a mountain : the top of a mountain. : 2 the highest level. : 3 a meeting or series of meetings between the leaders of two or more governments."
+    ]
+
+# encode queries and passages. The model produces unnormalized vectors. If your task requires normalized embeddings pass normalize_embeddings=True to encode as below.
+query_embeddings = model.encode(input_queries)
+passage_embeddings = model.encode(input_passages)
+
+# calculate cosine similarity
+print(util.cos_sim(query_embeddings, passage_embeddings))
